bcc140fb500189668369c42740ee230718e0c5e6
jschwabe <mail@jschwabe.site>
2024-01-17 13:27:11 +0000

    Fix echo command skipping arguments with -n option

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -7,8 +7,18 @@ char	*occurs_exclusively(const char *expected, const char *actual);
 static int	echo_default(const char **cmd_arr, size_t writelen)
 {
 	size_t	n_args;
+	size_t	skip_n;
 
 	n_args = arr_len(cmd_arr);
+	skip_n = 0;
+	if (writelen == 1 && (cmd_arr + writelen) && *(cmd_arr + writelen))
+	{
+		while (cmd_arr + writelen + skip_n
+			&& occurs(*(cmd_arr + writelen + skip_n), "-n")
+				&& ft_strncmp(*(cmd_arr + 1), "-n", 2) == 0)
+				skip_n++;
+		writelen += skip_n;
+	}
 	while (cmd_arr + writelen && *(cmd_arr + writelen) && writelen <= n_args)
 	{
 		ft_printf(*(cmd_arr + writelen));
@@ -23,8 +33,9 @@ int	echo(const char **cmd_arr)
 {
 	if (!cmd_arr || !*cmd_arr || !*(cmd_arr + 1))
 		return (write(1, "\n", 1));
-	if (occurs_exclusively("-n", *(cmd_arr + 1)))
-		return (echo_default(cmd_arr + 1, 1));
+	if (occurs(*(cmd_arr + 1), "-n")
+		&& ft_strncmp(*(cmd_arr + 1), "-n", 2) == 0)
+			return (echo_default(cmd_arr + 1, 1));
 	echo_default((cmd_arr + 1), 0);
 	write(1, "\n", 1);
 	return (0);
