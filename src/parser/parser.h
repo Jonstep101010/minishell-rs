56d277ca0bbfc0352f8cccf2e8421ea0b12939e8
jschwabe <mail@jschwabe.site>
2024-01-30 17:58:13 +0000

    Refactor split_outside_quotes function and add struct t_splitter
    Signed-off-by: jschwabe <mail@jschwabe.site>

--- src/parser/parser.h
+++ src/parser/parser.h
@@ -4,6 +4,16 @@
 # include "struct.h"
 int		parser(t_shell *shell);
 
+typedef struct s_splitter
+{
+	int		quote;
+	size_t	i;
+	size_t	start;
+	size_t	len;
+	char	**ret;
+	char	*tmp;
+}	t_splitter;
+
 char	**split_command(t_shell *shell);
 
 typedef struct s_expander
--- src/parser/split_command.c
+++ src/parser/split_command.c
@@ -1,4 +1,4 @@
-#include "ft_printf.h"
+#include "parser.h"
 #include "libft.h"
 #include "struct.h"
 #include "utils.h"
@@ -15,59 +15,70 @@
 // (childs will handle their own splitting)
 
 // check for spaces if no pipes, otherwise childs handle it
+
+static void	init_splitter(t_splitter *split, char *to_split)
+{
+	split->quote = 0;
+	split->i = 0;
+	split->start = 0;
+	split->len = ft_strlen(to_split);
+	split->ret = NULL;
+}
+
+static char	**split_iterator(t_splitter *split, char *to_split, char c)
+{
+	while (to_split[split->i] && split->start < split->len)
+	{
+		if (split->quote == 0 &&
+			(to_split[split->i] == '\'' || to_split[split->i] == '"'))
+				split->quote = to_split[split->i];
+		else if (split->quote != 0 && to_split[split->i] == split->quote)
+			split->quote = 0;
+		else if (split->quote == 0 && to_split[split->i] == c)
+		{
+			split->tmp = ft_substr(to_split, split->start,
+					split->i - split->start);
+			if (!split->tmp)
+				return (NULL);
+			split->ret = append_str_arr(split->ret, split->tmp);
+			free(split->tmp);
+			if (!split->ret)
+				return (NULL);
+			split->start = split->i + 1;
+		}
+		split->i++;
+	}
+	return (split->ret);
+}
+
 char	**split_outside_quotes(char *to_split, char c)
 {
-	int		quote;
-	size_t	i;
-	size_t	start;
-	size_t	len;
-	char	**ret;
-	char	*tmp;
+	t_splitter	split;
 
-	quote = 0;
-	i = 0;
-	start = 0;
 	if (!to_split)
 		return (NULL);
-	len = ft_strlen(to_split);
-	ret = NULL;
-	while (to_split[i] && start < len)
-	{
-		if (quote == 0 && (to_split[i] == '\'' || to_split[i] == '"'))
-			quote = to_split[i];
-		else if (quote != 0 && to_split[i] == quote)
-			quote = 0;
-		else if (quote == 0 && to_split[i] == c)
-		{
-			tmp = ft_substr(to_split, start, i - start);
-			ft_printf("%s\n", tmp);
-			ret = append_str_arr(ret, tmp);
-			if (!ret || !tmp)
-				return (NULL);
-			free(tmp);
-			start = i + 1;
-		}
-		i++;
-	}
-	tmp = ft_substr(to_split, start, i - (start));
-	ft_printf("%s\n", tmp);
-	ret = append_str_arr(ret, tmp);
-	if (!ret)
+	init_splitter(&split, to_split);
+	if (!split_iterator(&split, to_split, c))
 		return (NULL);
-	free(tmp);
-	return (ret);
+	split.tmp = ft_substr(to_split,
+		split.start, split.i - split.start);
+	ft_printf("%s\n", split.tmp);
+	split.ret = append_str_arr(split.ret, split.tmp);
+	if (!split.ret)
+		return (NULL);
+	free(split.tmp);
+	return (split.ret);
 }
 
 char	**split_command(t_shell *shell)
 {
 	shell->tokens = split_outside_quotes(shell->line, '|');
-	// exit(0);
 	if (!shell->tokens)
 		return (NULL);
 	print_arr(shell->tokens);
 	// @todo this would usually take place in the childs
 	shell->command = split_outside_quotes(
-		shell->tokens[2], ' ');
+		shell->tokens[0], ' ');
 	print_arr_sep(shell->command, '{', '}');
 	arr_free(shell->tokens);
 	return (shell->tokens);
