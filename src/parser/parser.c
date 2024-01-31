cca156a5062d9bf38d10fb252075eb13658c4f90
jschwabe <mail@jschwabe.site>
2024-01-31 19:31:12 +0000

    enable command parsing
    Signed-off-by: jschwabe <mail@jschwabe.site>

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -22,14 +22,14 @@ int		parser(t_shell *shell)
 	if (!shell->tokens)
 		return (free(shell->expanded_line), EXIT_FAILURE);
 	// 	return (print_arr_sep(shell->tokens, '{', '}'), EXIT_FAILURE);
-	arr_free(shell->tokens);
-	free(shell->expanded_line);
 	// @audit give to childs everything after shell->tokens[0]
 	// split for ourselves
-	// shell->command = split_outside_quotes(
-	// 	shell->tokens[0], ' ');
+	shell->command = split_outside_quotes(
+		shell->tokens[0], ' ');
+	arr_free(shell->tokens);
+	free(shell->expanded_line);
 	// print_arr_sep(shell->command, '{', '}');
-	// if (!interpret_quotes(shell->command))
-	// 	return (EXIT_FAILURE);
+	if (!interpret_quotes(shell->command))
+		return (EXIT_FAILURE);
 	return (EXIT_SUCCESS);
 }
