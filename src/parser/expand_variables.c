bc4056a6eee76d48a20b3ea491736adf8ae40c41
jschwabe <mail@jschwabe.site>
2024-01-31 19:16:06 +0000

    refactor variable expansion
    still not all functionality available:
    - unterminated quotes in var handling
    - Keys in parentheses
    
    Signed-off-by: jschwabe <mail@jschwabe.site>

--- src/parser/expand_variables.c
+++ src/parser/expand_variables.c
@@ -1,5 +1,6 @@
 #include "env.h"
 #include "libft.h"
+#include "parser.h"
 #include <stdbool.h>
 
 static bool	is_valid_key(int c)
@@ -9,75 +10,80 @@ static bool	is_valid_key(int c)
 	return (false);
 }
 
+static char *replacer(t_expander *x, const char **envp)
+{
+	if (!x->ret)
+		return (NULL);
+	if (x->i <= ft_strlen(x->line))
+	{
+		x->remainder_line = ft_strdup(&x->line[x->i]);
+		free(x->line);
+		if (!x->remainder_line)
+			return (x->ret);
+		printf("remainder: %s\n", x->remainder_line);
+		x->tmp = ft_strjoin(x->ret, x->remainder_line);
+		free(x->remainder_line);
+		free(x->ret);
+		x->ret = expand_variables(x->tmp, envp);
+		free(x->tmp);
+	}
+	return (x->ret);
+}
+
+static char	*insert_value(t_expander *x, const char **envp)
+{
+	x->key = ft_substr(x->line, x->start, x->i - x->start);
+	if (!x->key)
+		return (NULL);
+	x->val = get_var_val(envp, x->key);
+	free(x->key);
+	if (!x->val)
+		return (NULL);
+	x->tmp = ft_substr(x->line, 0, x->start - 1);
+	if (!x->tmp)
+		return (free(x->val), NULL);
+	x->ret = ft_strjoin(x->tmp, x->val);
+	free(x->tmp);
+	free(x->val);
+	return (x->ret);
+}
+
+static char	*check_line(t_expander *x, const char **envp)
+{
+	while (x->line && x->line[x->i])
+	{
+		if (x->line[x->i] == '\'' && x->singlequote == 0)
+			x->singlequote = x->line[x->i];
+		else if (x->line[x->i] == '\'' && x->singlequote == x->line[x->i])
+			x->singlequote = 0;
+		else if (x->line[x->i] && x->line[x->i + 1] && x->line[x->i] == '$'
+			&& x->singlequote == 0 && is_valid_key(x->line[x->i + 1]))
+		{
+			x->i++;
+			x->start = x->i;
+			while (x->line[x->i] && is_valid_key(x->line[x->i]))
+				x->i++;
+			if (x->start == x->i)
+				continue;
+			if (!insert_value(x, envp))
+				return (NULL);
+			return (replacer(x, envp));
+		}
+		if (x->line[x->i] == '$' && x->singlequote == 0)
+			x->line[x->i] = ' ';
+		x->i++;
+	}
+	return (x->line);
+}
+
+// @follow-up handle in parentheses
+/* @todo correct behaviour with quotes (pass in from top, same in recursion (shared state))*/
 char	*expand_variables(const char *input, const char **envp)
 {
-	size_t	i;
-	char	*ret;
-	char	*key;
-	size_t	start;
-	char	*val;
-	char	*tmp;
-	int		singlequote;
-	char	*remainder_line;
+	t_expander	x;
 
-	i = 0;
-	singlequote = 0;
-	char	*line = ft_strdup(input);
-	if (!line)
-		return (NULL);
-	while (line[i])
-	{
-		if (line[i] == '\'' && singlequote == 0)
-			singlequote = line[i];
-		else if (line[i] == '\'' && singlequote == line[i])
-			singlequote = 0;
-		else if (line[i] && line[i + 1] && line[i] == '$'
-			&& singlequote == 0 && is_valid_key(line[i + 1]))
-		{
-			i++;
-			start = i;
-			// iterate over key
-			while (line[i] && is_valid_key(line[i]))
-				i++;
-			// if no key, continue looping
-			if (start == i)
-				continue;
-			// get key
-			key = ft_substr(line, start, i - start);
-			if (!key)
-				return (NULL);
-			val = get_var_val(envp, key);
-			free(key);
-			if (!val)
-				return (NULL);
-			tmp = ft_substr(line, 0, start - 1);
-			if (!tmp)
-				return (free(val), NULL);
-			ret = ft_strjoin(tmp, val);
-			free(tmp);
-			free(val);
-			if (!ret)
-				return (NULL);
-			// if not done looping the line -> replace ret after line index with line
-			if (i <= ft_strlen(line))
-			{
-				remainder_line = ft_strdup(&line[i]);
-				free(line);
-				if (!remainder_line)
-					return (ret);
-				printf("remainder: %s\n", remainder_line);
-				tmp = ft_strjoin(ret, remainder_line);
-				free(remainder_line);
-				free(ret);
-				ret = expand_variables(tmp, envp);
-				free(tmp);
-			}
-			printf("expanded now: %s\n", ret);
-			return (ret);
-		}
-		if (line[i] == '$' && singlequote == 0)
-			line[i] = ' ';
-		i++;
-	}
-	return (line);
+	x.i = 0;
+	x.singlequote = 0;
+	x.line = ft_strdup(input);
+	return (check_line(&x, envp));
 }
--- src/parser/parser.h
+++ src/parser/parser.h
@@ -4,6 +4,19 @@
 # include "struct.h"
 int		parser(t_shell *shell);
 
+typedef struct s_expander
+{
+	size_t	i;
+	char	*ret;
+	char	*key;
+	size_t	start;
+	char	*val;
+	char	*tmp;
+	int		singlequote;
+	char	*remainder_line;
+	char	*line;
+}	t_expander;
+
 typedef struct s_splitter
 {
 	int		quote;
@@ -15,7 +28,7 @@ typedef struct s_splitter
 	char	**arr;
 }	t_splitter;
 
-char	*expand_variables(const char *line, const char **envp);
+char	*expand_variables(const char *input, const char **envp);
 
 bool	interpret_quotes(char **cmd_arr);
 
