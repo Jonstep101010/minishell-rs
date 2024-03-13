4bb9a6909a57f105d5f4cd88598b9c07e194551e
jschwabe <mail@jschwabe.site>
2024-03-13 11:56:01 +0000

    Refactor expand to contain entire module
    Signed-off-by: jschwabe <mail@jschwabe.site>

--- src/expander/expand.c
+++ src/expander/expand.c
@@ -61,7 +61,7 @@ static void	check_quotes(const char *s, bool *expand, int *double_quote)
  * @param env
  * @return char*
  */
-char	*expand(char const *s, char *const *env)
+static char	*expand(char const *s, char *const *env)
 {
 	int		i;
 	bool	expand;
@@ -88,4 +88,26 @@ char	*expand(char const *s, char *const *env)
 			ret = append_char_str(ret, s[i]);
 	}
 	return (ret);
+}
+
+/**
+ * @brief iterate over to find key
+ * @details if key found, replace range of $key with value
+ * \details if key contains $key, expand only its string to value
+ * \details else iterate over to find next key
+ * \details if there are $key$key_two, expand each key separately
+ * \details if the expansion of $key results in key, do not expand
+ * \details $invalid$key$keytwo -> valuevaluetwo (invalid empty)
+ * @example input: $$USER -> $username
+ * @param input_expander
+ * @param env
+ * @return char*
+ */
+char	*expander(char const *input_expander, char *const *env)
+{
+	if (!input_expander || !env || !*env || !*input_expander)
+		return (NULL);
+	if (ft_strchr(input_expander, '$') == NULL)
+		return (ft_strdup(input_expander));
+	return (expand(input_expander, env));
 }
--- src/expander/expander.c
+++ src/expander/expander.c
@@ -3,26 +3,3 @@
 #include <stdio.h>
 #include <sys/param.h>
 
-char	*expand(char const *input, char *const *env);
-
-/**
- * @brief iterate over to find key
- * @details if key found, replace range of $key with value
- * \details if key contains $key, expand only its string to value
- * \details else iterate over to find next key
- * \details if there are $key$key_two, expand each key separately
- * \details if the expansion of $key results in key, do not expand
- * \details $invalid$key$keytwo -> valuevaluetwo (invalid empty)
- * @example input: $$USER -> $username
- * @param input_expander
- * @param env
- * @return char*
- */
-char	*expander(char const *input_expander, char *const *env)
-{
-	if (!input_expander || !env || !*env || !*input_expander)
-		return (NULL);
-	if (ft_strchr(input_expander, '$') == NULL)
-		return (ft_strdup(input_expander));
-	return (expand(input_expander, env));
-}
