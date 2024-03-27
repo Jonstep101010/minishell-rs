fcbf9f4999044a9101c629654e5183a0b03e7adf
jschwabe <mail@jschwabe.site>
2024-03-27 22:51:51 +0000

    move exec_bin to own file
    Signed-off-by: jschwabe <mail@jschwabe.site>

--- src/execution/execute_commands.c
+++ src/execution/execute_commands.c
@@ -11,55 +11,13 @@
 #include <unistd.h>
 #include "execution.h"
 
-uint8_t	set_binpath(char *const *env, const char *bin, char **binpath_buf);
-
-/**
- * @brief is called by single command/pipe to execute the command
- * @audit should be renamed
- * @param shell
- * @param token pointer to element in array of tokens
- */
-int	exec_bin(t_shell *shell, t_token *token)
-{
-	// @todo implement signals
-	int			access_status;
-	const char	**command = (const char **)get_cmd_arr_token(token);
-
-	if (!command)
-		exit_free(shell, 0);
-	access_status = set_binpath(shell->env, token->cmd_args[0].elem, &(token->bin));
-	if (access_status == 1 || access_status == 126 || access_status == 127)
-		arr_free((char **)command);
-	if (access_status == 1)
-	{
-		eprint("alloc fail");
-		exit_free(shell, 1);
-	}
-	if (access_status == 126)
-	{
-		// eprint("%s: %s", token->cmd_args[0].elem, strerror(errno));
-		exit_free(shell, 126);
-	}
-	if (access_status == 127)
-	{
-		eprint("command not found: %s", token->cmd_args[0].elem);
-		exit_free(shell, 127);
-	}
-	if (execve(token->bin, (char **)command, shell->env) == -1)
-	{
-		arr_free((char **)command);
-		execve_fail(shell, token->bin);
-	}
-	exit_free(shell, 0);
-	return (0);
-}
-
 size_t	memsize(void *arr, size_t size);
 
 static bool forkable_builtin(t_token *token)
 {
 	return (token->cmd_func != builtin_exit
-		&& token->cmd_func != builtin_export && token->cmd_func != builtin_unset
+		&& token->cmd_func != builtin_export
+		&& token->cmd_func != builtin_unset
 		&& token->cmd_func != builtin_cd);
 }
 
