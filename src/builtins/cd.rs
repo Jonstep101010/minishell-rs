fe917fe1de52a524d90724bdb36b72123a17bb5e
Jonstep101010 <mail@jschwabe.site>
2024-11-04 21:27:22 +0100

    chore: partially modify cd to use rust features

--- src/builtins/cd.rs
+++ src/builtins/cd.rs
@@ -1,109 +1,86 @@
 use ::libc;
+use libft_rs::ft_strdup::ft_strdup;
 
 use crate::{
-	__errno_location,
 	environment::{export_env::export_env, get_env::get_env},
 	t_shell, t_token,
 	tokenizer::build_command::get_cmd_arr_token,
-	utils::error::eprint,
 };
-use libutils_rs::src::{
-	array::arr_free::arr_free,
-	string::join_strings::{free_first_join, free_second_join},
-	utils::free_mem::free_null,
-};
+use libutils_rs::src::{array::arr_free::arr_free, utils::free_mem::free_null};
+use std::ffi::{CStr, CString};
 
-use libc::{chdir, free, getcwd, strerror};
-
-unsafe extern "C" fn changedir(
-	mut path: *const libc::c_char,
-	mut shell: *mut t_shell,
-) -> libc::c_int {
-	let mut oldpwd: *mut libc::c_char = getcwd(
-		std::ptr::null_mut::<libc::c_char>(),
-		0 as libc::c_int as usize,
-	);
-	if chdir(path) == -(1 as libc::c_int) {
-		eprint(
-			b"cd: %s: %s\0" as *const u8 as *const libc::c_char,
-			path,
-			strerror(*__errno_location()),
-		);
-		free(oldpwd as *mut libc::c_void);
-		return -(1 as libc::c_int);
+fn changedir(mut rust_path: &str, mut shell: *mut t_shell) -> bool {
+	let oldpwd = std::env::current_dir().unwrap();
+	match std::env::set_current_dir(std::path::Path::new(rust_path)) {
+		Ok(_) => {
+			let pwd = std::env::current_dir();
+			match pwd {
+				Ok(p) => {
+					let pwd_prefixed = String::from("PWD=") + p.to_str().unwrap();
+					let old_prefixed = String::from("OLDPWD=") + oldpwd.to_str().unwrap();
+					unsafe {
+						export_env(
+							shell,
+							ft_strdup(CString::new(pwd_prefixed).unwrap().as_ptr()),
+						);
+						export_env(
+							shell,
+							ft_strdup(CString::new(old_prefixed).unwrap().as_ptr()),
+						);
+					}
+					true
+				}
+				Err(e) => {
+					eprintln!("cd: {}: {}", rust_path, e);
+					false
+				}
+			}
+		}
+		Err(e) => {
+			eprintln!("cd: {}: {}", rust_path, e);
+			false
+		}
 	}
-	let mut pwd: *mut libc::c_char = getcwd(
-		std::ptr::null_mut::<libc::c_char>(),
-		0 as libc::c_int as usize,
-	);
-	if pwd.is_null() {
-		eprint(
-			b"cd: %s: %s\0" as *const u8 as *const libc::c_char,
-			path,
-			strerror(*__errno_location()),
-		);
-		free(oldpwd as *mut libc::c_void);
-		return -(1 as libc::c_int);
-	}
-	export_env(
-		shell,
-		free_second_join(b"PWD=\0" as *const u8 as *const libc::c_char, pwd),
-	);
-	export_env(
-		shell,
-		free_second_join(b"OLDPWD=\0" as *const u8 as *const libc::c_char, oldpwd),
-	);
-	0 as libc::c_int
 }
-unsafe extern "C" fn cd_internal(
-	mut cmd_args: *mut *const libc::c_char,
-	mut shell: *mut t_shell,
-) -> libc::c_int {
+unsafe extern "C" fn cd_internal(mut opt_cmd_args: Option<&CStr>, mut shell: *mut t_shell) -> i32 {
 	let mut path: *mut libc::c_char =
 		get_env((*shell).env, b"HOME\0" as *const u8 as *const libc::c_char);
 	let mut oldpwd: *mut libc::c_char = get_env(
 		(*shell).env,
 		b"OLDPWD\0" as *const u8 as *const libc::c_char,
 	);
-	if (*cmd_args.offset(1 as libc::c_int as isize)).is_null() && path.is_null() {
+	if opt_cmd_args.is_none() && path.is_null() {
 		free_null(&mut oldpwd as *mut *mut libc::c_char as *mut libc::c_void);
-		eprint(b"cd: HOME not set\0" as *const u8 as *const libc::c_char);
-		return 1 as libc::c_int;
+		eprintln!("cd: HOME not set");
+		return 1;
 	}
-	if (*cmd_args.offset(1 as libc::c_int as isize)).is_null() && !path.is_null() {
-		changedir(path, shell);
-	} else if **cmd_args.offset(1 as libc::c_int as isize) as libc::c_int == '~' as i32
-		&& !path.is_null()
-	{
-		path = free_first_join(
-			path,
-			(*cmd_args.offset(1 as libc::c_int as isize)).offset(1 as libc::c_int as isize),
-		);
-		if changedir(path, shell) == -(1 as libc::c_int) {
-			free(path as *mut libc::c_void);
-			free(oldpwd as *mut libc::c_void);
+	if opt_cmd_args.is_none() && !path.is_null() {
+		let strref = CStr::from_ptr(path).to_str().unwrap();
+		changedir(strref, shell);
+	} else if !path.is_null() && opt_cmd_args.unwrap().to_bytes() == b"~" {
+		let strref = CStr::from_ptr(path).to_str().unwrap();
+		println!("{}", strref);
+		if !changedir(strref, shell) {
+			libc::free(path as *mut libc::c_void);
+			libc::free(oldpwd as *mut libc::c_void);
 			return 1 as libc::c_int;
 		}
-	} else if **cmd_args.offset(1 as libc::c_int as isize) as libc::c_int == '-' as i32
-		&& !oldpwd.is_null()
-	{
-		oldpwd = free_first_join(
-			oldpwd,
-			(*cmd_args.offset(1 as libc::c_int as isize)).offset(1 as libc::c_int as isize),
-		);
-		if changedir(oldpwd, shell) == -(1 as libc::c_int) {
-			free(path as *mut libc::c_void);
-			free(oldpwd as *mut libc::c_void);
+	} else if opt_cmd_args.unwrap().to_bytes() == b"-" && !oldpwd.is_null() {
+		let strref = CStr::from_ptr(oldpwd).to_str().unwrap();
+		println!("{}", strref);
+		if !changedir(strref, shell) {
+			libc::free(path as *mut libc::c_void);
+			libc::free(oldpwd as *mut libc::c_void);
 			return 1 as libc::c_int;
 		}
-	} else if changedir(*cmd_args.offset(1 as libc::c_int as isize), shell) == -(1 as libc::c_int) {
-		free(path as *mut libc::c_void);
-		free(oldpwd as *mut libc::c_void);
-		return 1 as libc::c_int;
+	} else if !changedir(opt_cmd_args.unwrap().to_str().unwrap(), shell) {
+		libc::free(path as *mut libc::c_void);
+		libc::free(oldpwd as *mut libc::c_void);
+		return 1;
 	}
-	free(path as *mut libc::c_void);
-	free(oldpwd as *mut libc::c_void);
-	0 as libc::c_int
+	libc::free(path as *mut libc::c_void);
+	libc::free(oldpwd as *mut libc::c_void);
+	0
 }
 #[no_mangle]
 pub unsafe extern "C" fn builtin_cd(
@@ -112,7 +89,14 @@ pub unsafe extern "C" fn builtin_cd(
 ) -> libc::c_int {
 	let mut command: *mut *const libc::c_char =
 		get_cmd_arr_token(token) as *mut *const libc::c_char;
-	let status = cd_internal(command, shell);
+	let cmd_args = *command.offset(1);
+	// option of cmd_args -> none if null
+	let opt_cmd_args = if cmd_args.is_null() {
+		None
+	} else {
+		Some(CStr::from_ptr(cmd_args))
+	};
+	let status = cd_internal(opt_cmd_args, shell);
 	arr_free(command as *mut *mut libc::c_char);
 	status
 }
