2f1fa5ed7ce8e7e0f4dba5fb34bacbc0dc85d487
jschwabe <mail@jschwabe.site>
2025-01-11 10:45:40 +0100

    WIP: unbloat check_key
    Signed-off-by: jschwabe <mail@jschwabe.site>

--- src/environment/check_key.rs
+++ src/environment/check_key.rs
@@ -1,24 +1,18 @@
-use ::libc;
+use crate::prelude::*;
 use libft_rs::{ft_isalnum::ft_isalnum, ft_isalpha::ft_isalpha};
-#[unsafe(no_mangle)]
-pub unsafe extern "C" fn check_valid_key(mut s: *const libc::c_char) -> bool {
-	let mut i: libc::c_int = 0;
-	if *s as libc::c_int == '=' as i32
-		|| !(ft_isalpha(*s as libc::c_int) != 0 || *s as libc::c_int == '_' as i32)
-	{
+
+pub unsafe fn check_valid_key(s: *const c_char) -> bool {
+	let mut i = 0;
+	let s: *const u8 = s.cast();
+	if *s == b'=' || !(ft_isalpha(*s as c_int) != 0 || *s == b'_') {
 		return false;
 	}
-	while *s.offset(i as isize) as libc::c_int != 0
-		&& *s.offset(i as isize) as libc::c_int != '=' as i32
-	{
-		if ft_isalnum(*s.offset(i as isize) as libc::c_int) != 0
-			|| *s.offset(i as isize) as libc::c_int == '_' as i32
-		{
+	while *s.offset(i) != 0 && *s.offset(i) != b'=' {
+		if ft_isalnum(*s.offset(i) as c_int) != 0 || *s.offset(i) == b'_' {
 			i += 1;
 		} else {
 			return false;
 		}
 	}
-	!(*s.offset(i as isize) as libc::c_int == '=' as i32
-		&& *s.offset((i + 1 as libc::c_int) as isize) as libc::c_int == '=' as i32)
+	!(*s.offset(i) == b'=' && *s.offset(i + 1) == b'=')
 }
