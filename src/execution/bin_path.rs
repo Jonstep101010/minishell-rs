ad8a1ea72d308fa01dc623803a8a1aa579506337
jschwabe <mail@jschwabe.site>
2025-01-11 18:07:31 +0100

    chore: fix empty input bin_path
    Signed-off-by: jschwabe <mail@jschwabe.site>

--- src/execution/bin_path.rs
+++ src/execution/bin_path.rs
@@ -49,30 +49,33 @@ fn set_single(bin: &CStr, env: &Env) -> (u8, Option<CString>) {
 }
 
 pub fn get_path_prefixed(env: &Env, bin: &CStr) -> (u8, Option<CString>) {
-	assert!(!bin.is_empty());
+	if bin.is_empty() {
+		return (127, None);
+	}
 	let bin_bytes = bin.to_bytes();
-	if bin_bytes.len() == 1
-		&& bin_bytes
+	if bin_bytes.len() == 1 {
+		if bin_bytes
 			.iter()
 			.any(|&c| c == b'/' || c == b'.' || c == b'~' || c == b'\0')
-	{
-		return set_single(bin, env);
-	}
-	if bin_bytes[0] == b'.' || bin_bytes[0] == b'/' {
-		let binpath = CString::from(bin);
-		if bin_bytes.len() == 2 && bin_bytes == b".." {
-			return (127, None);
+		{
+			return set_single(bin, env);
 		}
-		if binpath.is_empty() {
-			return (1, None);
+		if bin_bytes[0] == b'.' || bin_bytes[0] == b'/' {
+			let binpath = CString::from(bin);
+			if bin_bytes.len() == 2 && bin_bytes == b".." {
+				return (127, None);
+			}
+			if binpath.is_empty() {
+				return (1, None);
+			}
+			if nix::unistd::access(binpath.as_c_str(), AccessFlags::F_OK).is_err() {
+				return (127, None);
+			}
+			if nix::unistd::access(binpath.as_c_str(), AccessFlags::X_OK).is_err() {
+				return (126, None);
+			}
+			return (0, Some(binpath));
 		}
-		if nix::unistd::access(binpath.as_c_str(), AccessFlags::F_OK).is_err() {
-			return (127, None);
-		}
-		if nix::unistd::access(binpath.as_c_str(), AccessFlags::X_OK).is_err() {
-			return (126, None);
-		}
-		return (0, Some(binpath));
 	}
 	get_bin(bin, env.get_paths())
 }
