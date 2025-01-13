3d79c88f0f880c2a0b2a4325e4ab01a912982a46
jschwabe <mail@jschwabe.site>
2025-01-13 12:17:35 +0100

    WIP: fixed expansion
    Signed-off-by: jschwabe <mail@jschwabe.site>

--- src/environment/expander.rs
+++ src/environment/expander.rs
@@ -1,90 +1,121 @@
 #![warn(clippy::pedantic)]
+use libc::size_t;
+
 use super::Env;
 use std::ffi::{CStr, CString};
 
 const CHARMATCH: &[u8; 9] = b"$\"'/? )(\0";
-fn check_index_advance(bytes_s: &[u8], mut i: usize) -> usize {
+
+fn check_index_advance(bytes_s_at_i: &[u8]) -> usize {
 	let mut count: usize = 0;
-	while bytes_s[i + count] != b'\0'
-		&& bytes_s[i + count + 1] != b'\0'
-		&& !CHARMATCH.iter().any(|&x| x == bytes_s[i + count + 1])
+	while bytes_s_at_i[count] != b'\0'
+		&& bytes_s_at_i[count + 1] != b'\0'
+		&& !CHARMATCH.iter().any(|&x| x == bytes_s_at_i[count + 1])
 	{
 		count += 1;
 	}
-	if bytes_s[i + count] != b'\0' && bytes_s[i + count + 1] == b'?' {
+	if bytes_s_at_i[count] != b'\0' && bytes_s_at_i[count + 1] == b'?' {
 		count += 1;
 	}
 	count
 }
+
 fn expand_inside(key_c_str: &CStr, env: &Env, mut i: &mut usize) -> String {
-	let mut ret: String = {
-		if !key_c_str.is_empty() {
-			if let Some(expansion) = env.get(key_c_str.to_str().unwrap()) {
-				expansion.to_string()
-			} else {
-				"$".to_string()
-			}
-		} else {
-			String::from("")
-		}
-	};
-	*i = (*i).wrapping_add(key_c_str.count_bytes());
-	ret
+	// advance by key length in source string
+	*i += key_c_str.count_bytes();
+	if key_c_str.is_empty() {
+		"$".to_string()
+	} else if let Some(expansion) = env.get(key_c_str.to_str().unwrap()) {
+		expansion.to_string()
+	} else {
+		String::from("")
+	}
 }
+
+unsafe fn expand_inside_c(
+	mut key: *mut libc::c_char,
+	mut env: &Env,
+	mut i: *mut libc::c_int,
+) -> *mut libc::c_char {
+	let len: size_t = libc::strlen(key);
+	let mut ret = std::ptr::null_mut::<libc::c_char>();
+	let cstr = CStr::from_ptr(key);
+	if *key != 0 {
+		if let Some(val) = env.get(cstr.to_str().unwrap()) {
+			let cstr = CString::new(val.as_str()).expect("valid cstring from val");
+			ret = libft_rs::ft_strdup(cstr.as_ptr());
+		}
+	} else {
+		ret = libft_rs::ft_strdup(c"$".as_ptr());
+	}
+	if ret.is_null() {
+		ret = libft_rs::ft_strdup(b"\0" as *const u8 as *const libc::c_char);
+	}
+	libc::free(key as *mut libc::c_void);
+	if ret.is_null() {
+		return 0 as *mut libc::c_char;
+	}
+	*i = (*i as libc::c_ulong).wrapping_add(len as u64) as libc::c_int as libc::c_int;
+	return ret;
+}
+
 fn check_quotes(c: &[u8], mut expand_0: &mut bool, mut double_quote: &mut i32) {
 	if c[0] == b'"' && *double_quote == 0 {
 		*double_quote = 1;
 	} else if c[0] == b'"' && *double_quote == 1 {
 		*double_quote = 0;
 	}
-	if c[0] == b'\'' && *double_quote == 0 && c[check_index_advance(c, 0)] != b'"' {
+	if c[0] == b'\'' && *double_quote == 0 && c[check_index_advance(&c[0..])] != b'"' {
 		*expand_0 = !*expand_0;
 	}
 }
 const EXP_CHARS: &[u8; 4] = b"$()\0";
-fn expand(s: &CStr, env: &Env) -> CString {
-	let mut i: usize = 0;
-	let mut expand_0: bool = false;
-	let mut double_quote = 0;
-	let mut ret = String::new();
-	let bytes_s = s.to_bytes_with_nul();
-	loop {
-		if bytes_s[0] == b'\0' {
-			break;
-		}
-		check_quotes(&bytes_s[i..], &mut expand_0, &mut double_quote);
-		if !expand_0
-			&& bytes_s[i] == b'$'
-			&& bytes_s[i + 1] != b'\0'
-			&& !EXP_CHARS.iter().any(|&x| x == bytes_s[i + 1])
-		{
-			let start = i + 1;
-			let len = check_index_advance(bytes_s, i);
-			unsafe {
-				let key = CStr::from_bytes_with_nul_unchecked(&bytes_s[start..=(start + len)]);
-				let expansion = expand_inside(key, env, &mut i);
-				ret.push_str(&expansion);
-			}
-		} else if i < bytes_s.len() - 1 {
-			ret.push(bytes_s[i] as char);
-		} else {
-			break;
-		}
-		i += 1;
-	}
-	CString::new(ret).unwrap()
-}
 
 /// Expand the input string using the environment variables stored in the `env` struct.
 ///
 /// # Arguments
 /// `input_expander` - A `CStr` reference to the input string to be expanded.
 #[must_use]
-pub fn expander(input_expander: &CStr, env: &Env) -> Option<CString> {
+pub unsafe fn expander(input_expander: &CStr, env: &Env) -> Option<CString> {
 	if input_expander.is_empty() {
 		return None;
 	}
-	Some(expand(input_expander, env))
+	let mut i = 0;
+	let mut expand_0: bool = true;
+	let mut double_quote = 0;
+	let mut ret = String::from("");
+	let bytes_s = input_expander.as_ptr();
+	let b_s = input_expander.to_bytes_with_nul();
+	loop {
+		if *bytes_s.add(i) == b'\0' as i8 {
+			break;
+		}
+		check_quotes(&b_s[i..], &mut expand_0, &mut double_quote);
+		if expand_0
+			&& *bytes_s.add(i) == b'$' as i8
+			&& *bytes_s.add(i + 1) != b'\0' as i8
+			&& !EXP_CHARS.iter().any(|&x| x == *bytes_s.add(i + 1) as u8)
+		{
+			let mut key =
+				libft_rs::ft_substr(bytes_s, i as u32 + 1, check_index_advance(&b_s[i..]) as u64)
+					as *mut libc::c_char;
+			if key.is_null() {
+				return None;
+			}
+			let expansion = expand_inside(CStr::from_ptr(key), env, &mut i);
+			let expansion_c = expand_inside_c(key, env, &mut (i as i32));
+			assert_eq!(
+				*CStr::from_ptr(expansion_c),
+				*CString::new(expansion.clone()).unwrap().as_c_str()
+			);
+			libc::free(expansion_c.cast());
+			ret.push_str(&expansion);
+		} else {
+			ret.push(*bytes_s.add(i) as u8 as char);
+		}
+		i += 1;
+	}
+	Some(CString::new(ret).unwrap())
 }
 
 #[cfg(test)]
@@ -126,9 +157,11 @@ mod tests {
 	#[fixture]
 	fn test_expander(#[case] expected: &str, #[case] input: &str) {
 		let env = Env::new();
-		let input = CString::new(input).unwrap();
-		let output = expander(&input, &env).unwrap();
-		assert_eq!(expected, output.to_str().unwrap());
+		unsafe {
+			let input = CString::new(input).unwrap();
+			let output = expander(&input, &env).unwrap();
+			assert_eq!(expected, output.to_str().unwrap());
+		}
 	}
 	#[rstest]
 	// #[case("echo 0", "echo $?")]
@@ -138,13 +171,15 @@ mod tests {
 	#[case("echo $'TEST $TEST'", "echo $'TEST $TEST'")]
 	#[case("echo $\"42$\"", "echo $\"42$\"")]
 	#[case("echo \"$ \"", "echo \"$ \"")]
-	#[case(&format!("echo ${}$", std::env::var("USER").unwrap()), "echo $USER$")]
+	#[case(&format!("echo {}$", std::env::var("USER").unwrap()), "echo $USER$")]
 	#[case("echo something $$ strange", "echo something $$ strange")]
 	#[fixture]
 	fn test_expander_failing(#[case] expected: &str, #[case] input: &str) {
 		let env = Env::new();
-		let input = CString::new(input).unwrap();
-		let output = expander(&input, &env).unwrap();
-		assert_eq!(expected, output.to_str().unwrap());
+		unsafe {
+			let input = CString::new(input).unwrap();
+			let output = expander(&input, &env).unwrap();
+			assert_eq!(expected, output.to_str().unwrap());
+		}
 	}
 }
