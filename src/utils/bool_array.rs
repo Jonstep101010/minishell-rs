8deda2e7a2b0812ae1b624ae13283c2e756f6f17
jschwabe <mail@jschwabe.site>
2025-01-16 20:08:05 +0100

    WIP: create abstraction for allocation
    Signed-off-by: jschwabe <mail@jschwabe.site>

--- src/utils/bool_array.rs
+++ src/utils/bool_array.rs
@@ -19,25 +19,86 @@ pub unsafe fn bool_arr_zeroing(mut len: size_t) -> *mut bool {
 	ignore
 }
 
+trait BoolArray {
+	// fn zeroing_nul(len: usize) -> Self;
+	fn zeroing(len: usize) -> Self;
+	// fn range_ignore(s: &[u8], ignore: &mut Self, c: u8);
+	// fn range_ignore_nul(s: &[u8], ignore: &mut Self, c: u8);
+}
+
+impl BoolArray for Vec<bool> {
+	fn zeroing(len: usize) -> Self {
+		let mut vec = Vec::with_capacity(len + 1);
+		for _ in 0..=len {
+			vec.push(false);
+		}
+		vec
+	}
+}
+
+impl BoolArray for Box<[bool]> {
+	fn zeroing(len: usize) -> Self {
+		let mut vec = Vec::with_capacity(len + 1);
+		for _ in 0..=len {
+			vec.push(false);
+		}
+		vec.into_boxed_slice()
+	}
+}
+
+pub fn bool_arr_zeroing_vec(len: usize) -> Vec<bool> {
+	Vec::zeroing(len)
+}
+
+pub fn bool_arr_zeroing_box(len: usize) -> Box<[bool]> {
+	Box::zeroing(len)
+}
+
 pub unsafe fn range_ignore(
 	mut s: *const libc::c_char,
 	mut ignore: *mut bool,
 	mut c: libc::c_uchar,
 ) {
 	let mut i = 0;
-	while *s.offset(i) != 0 {
-		if *s.offset(i) as u8 == c && *ignore.offset(i) as u8 == 0 {
-			*ignore.offset(i) = 1 != 0;
+	while *s.add(i) != 0 {
+		if *s.add(i) as u8 == c && *ignore.add(i) as u8 == 0 {
+			*ignore.add(i) = true;
 			i += 1;
-			while *s.offset(i) != 0 && *s.offset(i) as u8 != c {
-				*ignore.offset(i) = 1 != 0;
+			while *s.add(i) != 0 && *s.add(i) as u8 != c {
+				*ignore.add(i) = true;
 				i += 1;
 			}
-			if *s.offset(i) as u8 == c {
-				*ignore.offset(i) = 1 != 0;
+			if *s.add(i) as u8 == c {
+				*ignore.add(i) = true;
 			}
-			while *s.offset(i) as u8 != c {
-				*ignore.offset(i) = false;
+			while *s.add(i) as u8 != c {
+				*ignore.add(i) = false;
+				i -= 1;
+			}
+		} else {
+			i += 1;
+		}
+	}
+}
+pub unsafe fn rs_range_ignore(
+	mut s: *const libc::c_char,
+	mut ignore: *mut bool,
+	mut c: libc::c_uchar,
+) {
+	let mut i = 0;
+	while *s.add(i) != 0 {
+		if *s.add(i) as u8 == c && *ignore.add(i) as u8 == 0 {
+			*ignore.add(i) = true;
+			i += 1;
+			while *s.add(i) != 0 && *s.add(i) as u8 != c {
+				*ignore.add(i) = true;
+				i += 1;
+			}
+			if *s.add(i) as u8 == c {
+				*ignore.add(i) = true;
+			}
+			while *s.add(i) as u8 != c {
+				*ignore.add(i) = false;
 				i -= 1;
 			}
 		} else {
@@ -51,6 +112,8 @@ mod tests {
 	use crate::utils::bool_array::{bool_arr_zeroing, libc::strlen, range_ignore};
 
 	use libc::size_t;
+
+	use super::{BoolArray, bool_arr_zeroing_box, bool_arr_zeroing_vec};
 
 	unsafe fn support_bool_arr_zeroing(mut ignore: *mut *mut bool, mut len: usize) {
 		*ignore = super::bool_arr_zeroing(len as u64) as *mut bool;
@@ -126,49 +189,59 @@ mod tests {
 	#[test]
 	fn test_ignore_can_work0() {
 		unsafe {
-			let mut s: [libc::c_char; 62] =
-				*::core::mem::transmute::<&[u8; 62], &mut [libc::c_char; 62]>(
-					b"this is my input \"ignore\" 't' 'this' \"is\" 'a' \"test\" 'string'\0",
-				);
+			let mut s = c"this is my input \"ignore\" 't' 'this' \"is\" 'a' \"test\" 'string'";
 			let _expected: *mut bool = support_expected(
 				b"00000000000000000111111110111011111101111011101111110111111110\0" as *const u8
 					as *const libc::c_char,
 			);
-			let mut len = libc::strlen(s.as_mut_ptr());
+			let mut len = libc::strlen(s.as_ptr());
 			let mut arr = bool_arr_zeroing(len as u64);
+			let mut vec_arr: Vec<bool> = BoolArray::zeroing(len);
+			let mut bool_box: Box<[bool]> = BoolArray::zeroing(len);
 			if arr.is_null() {
 				assert!(false);
 			}
-			range_ignore(s.as_mut_ptr(), arr, b'"');
-			range_ignore(s.as_mut_ptr(), arr, b'\'');
-			let mut i = 0;
-			while i < libc::strlen(s.as_mut_ptr()) {
-				i += 1;
-			}
+			range_ignore(s.as_ptr(), arr, b'"');
+			range_ignore(s.as_ptr(), arr, b'\'');
+			range_ignore(s.as_ptr(), vec_arr.as_mut_ptr(), b'"');
+			range_ignore(s.as_ptr(), vec_arr.as_mut_ptr(), b'\'');
+			range_ignore(s.as_ptr(), bool_box.as_mut_ptr(), b'"');
+			range_ignore(s.as_ptr(), bool_box.as_mut_ptr(), b'\'');
 			support_ranges_test0(arr);
+			support_ranges_test0(vec_arr.as_mut_ptr());
+			support_ranges_test0(bool_box.as_mut_ptr());
 			libc::free(arr as *mut libc::c_void);
+			libc::free(_expected as *mut libc::c_void);
 		}
 	}
 	#[test]
 	fn test_range_ignore_sq_0() {
 		unsafe {
-			let mut arr: *mut bool = 0 as *mut bool;
-			let mut s: [libc::c_char; 26] = *::core::mem::transmute::<
-				&[u8; 26],
-				&mut [libc::c_char; 26],
-			>(b"this is my input 'ignore'\0");
-			support_bool_arr_zeroing(&mut arr, libc::strlen(s.as_mut_ptr()));
+			let mut arr: *mut bool = std::ptr::null_mut();
+			let mut s = c"this is my input 'ignore'";
+			let len = libc::strlen(s.as_ptr());
+			support_bool_arr_zeroing(&mut arr, len);
+			let mut vec_arr = bool_arr_zeroing_vec(len);
+			let mut bool_box = bool_arr_zeroing_box(len);
 			let mut i = 0;
-			while i <= libc::strlen(s.as_mut_ptr()) {
+			while i <= len {
 				i += 1;
 			}
-			super::range_ignore(s.as_mut_ptr(), arr, b'\'');
+			range_ignore(s.as_ptr(), arr, b'\'');
+			range_ignore(s.as_ptr(), vec_arr.as_mut_ptr(), b'\'');
+			range_ignore(s.as_ptr(), bool_box.as_mut_ptr(), b'\'');
 			let mut i_0 = 0;
 			while i_0 <= 16 {
+				assert_eq!(false, *arr.add(i_0));
+				assert_eq!(false, *vec_arr.as_ptr().add(i_0));
+				assert_eq!(false, *bool_box.as_ptr().add(i_0));
 				i_0 += 1;
 			}
 			let mut i_1 = 17;
 			while i_1 <= 24 {
+				assert_eq!(true, *arr.add(i_1));
+				assert_eq!(true, *vec_arr.as_ptr().add(i_1));
+				assert_eq!(true, *bool_box.as_ptr().add(i_1));
 				i_1 += 1;
 			}
 			libc::free(arr as *mut libc::c_void);
@@ -177,23 +250,27 @@ mod tests {
 	#[test]
 	fn test_range_ignore_dq_0() {
 		unsafe {
-			let mut arr: *mut bool = 0 as *mut bool;
-			let mut s: [libc::c_char; 26] = *::core::mem::transmute::<
-				&[u8; 26],
-				&mut [libc::c_char; 26],
-			>(b"this is my input \"ignore\"\0");
-			support_bool_arr_zeroing(&mut arr, libc::strlen(s.as_mut_ptr()) as usize);
-			let mut i = 0;
-			while i <= libc::strlen(s.as_mut_ptr()) {
-				i += 1;
-			}
-			super::range_ignore(s.as_mut_ptr(), arr, b'"');
+			let mut arr: *mut bool = std::ptr::null_mut();
+			let mut s = c"this is my input \"ignore\"";
+			let len = libc::strlen(s.as_ptr());
+			support_bool_arr_zeroing(&mut arr, len);
+			let mut vec_arr = bool_arr_zeroing_vec(len);
+			let mut bool_box = bool_arr_zeroing_box(len);
+			range_ignore(s.as_ptr(), arr, b'"');
+			range_ignore(s.as_ptr(), vec_arr.as_mut_ptr(), b'"');
+			range_ignore(s.as_ptr(), bool_box.as_mut_ptr(), b'"');
 			let mut i_0 = 0;
 			while i_0 < 17 {
+				assert_eq!(false, *arr.add(i_0));
+				assert_eq!(false, *vec_arr.as_ptr().add(i_0));
+				assert_eq!(false, *bool_box.as_ptr().add(i_0));
 				i_0 += 1;
 			}
 			let mut i_1 = 17;
 			while i_1 <= 24 {
+				assert_eq!(true, *arr.add(i_0));
+				assert_eq!(true, *vec_arr.as_ptr().add(i_0));
+				assert_eq!(true, *bool_box.as_ptr().add(i_0));
 				i_1 += 1;
 			}
 			libc::free(arr as *mut libc::c_void);
@@ -206,17 +283,29 @@ mod tests {
 			let mut s = c"this is my input \"'ignore'\"";
 			let len = libc::strlen(s.as_ptr());
 			support_bool_arr_zeroing(&mut arr, len);
+			let mut vec_arr = bool_arr_zeroing_vec(len);
+			let mut bool_box = bool_arr_zeroing_box(len);
 			for i in 0..=len {
 				assert_eq!(false, *arr.add(i));
+				assert_eq!(false, *vec_arr.as_ptr().add(i));
+				assert_eq!(false, *bool_box.as_ptr().add(i));
 			}
 			range_ignore(s.as_ptr(), arr, b'"');
 			range_ignore(s.as_ptr(), arr, b'\'');
+			range_ignore(s.as_ptr(), vec_arr.as_mut_ptr(), b'"');
+			range_ignore(s.as_ptr(), vec_arr.as_mut_ptr(), b'\'');
+			range_ignore(s.as_ptr(), bool_box.as_mut_ptr(), b'"');
+			range_ignore(s.as_ptr(), bool_box.as_mut_ptr(), b'\'');
 			assert!(*arr.add(24));
 			for i in 0..17 {
 				assert_eq!(*arr.add(i), false);
 			}
 			for i in 17..24 {
 				assert_eq!(*arr.add(i), true);
+			}
+			for i in 0..24 {
+				assert_eq!(*arr.add(i), *vec_arr.as_ptr().add(i));
+				assert_eq!(*arr.add(i), *bool_box.as_ptr().add(i));
 			}
 			libc::free(arr.cast());
 		}
@@ -228,12 +317,24 @@ mod tests {
 			let mut s = c"\"'\"";
 			let len = libc::strlen(s.as_ptr());
 			support_bool_arr_zeroing(&mut arr, len);
+			let mut vec_arr = bool_arr_zeroing_vec(len);
+			let mut bool_box = bool_arr_zeroing_box(len);
 			range_ignore(s.as_ptr(), arr, b'\'');
 			range_ignore(s.as_ptr(), arr, b'\"');
+			range_ignore(s.as_ptr(), vec_arr.as_mut_ptr(), b'\'');
+			range_ignore(s.as_ptr(), vec_arr.as_mut_ptr(), b'"');
+			range_ignore(s.as_ptr(), bool_box.as_mut_ptr(), b'\'');
+			range_ignore(s.as_ptr(), bool_box.as_mut_ptr(), b'"');
 			let expected = support_expected(c"1110".as_ptr());
 			for i in 0..=len {
 				assert_eq!(*expected.add(i), *arr.add(i));
 			}
+			for i in 0..=len - 1 {
+				assert_eq!(*expected.add(i), *vec_arr.as_ptr().add(i));
+				assert_eq!(*expected.add(i), *bool_box.as_ptr().add(i));
+			}
+			libc::free(arr.cast());
+			libc::free(expected.cast());
 		}
 	}
 }
