62ba8e383657c5e4189a7b8135222150ed52baa6
jschwabe <mail@jschwabe.site>
2025-01-29 18:43:51 +0100

    chore: replace fork with safe wrapper in unistd
    Signed-off-by: jschwabe <mail@jschwabe.site>

--- src/execution/execute_pipes.rs
+++ src/execution/execute_pipes.rs
@@ -1,57 +1,67 @@
 #![allow(unsafe_op_in_unsafe_fn)]
 use ::libc;
-use libc::{close, dup2, fork, pipe, wait, waitpid};
+use nix::{
+	sys::wait::{WaitStatus, waitpid},
+	unistd::{ForkResult, fork},
+};
 
 use super::{eprint_msh, executor, heredoc::do_heredocs, redirections::do_redirections, t_shell};
 
 unsafe fn exec_last(shell: &mut t_shell, i: usize, prevpipe: *mut i32) {
-	let mut status: i32 = 0;
-	let cpid = fork();
-	if cpid == 0 {
-		// check_signals_child(&mut (*shell).p_termios);
-		if shell.token_vec[i].has_redir {
-			do_heredocs(&shell.token_vec[i], &mut *prevpipe, &shell.env);
+	match unsafe { fork() } {
+		Ok(ForkResult::Parent { child }) => match waitpid(child, None) {
+			Ok(WaitStatus::Exited(_, exit_code)) => {
+				shell.env.set_status(exit_code);
+			}
+			Ok(WaitStatus::Signaled(_, signal, _)) => {
+				shell.env.set_status(128 + signal as i32);
+			}
+			Err(e) => eprintln!("waitpid failed: {}", e),
+			_ => (),
+		},
+		Ok(ForkResult::Child) => {
+			// check signals child
+			if shell.token_vec[i].has_redir {
+				do_heredocs(&shell.token_vec[i], &mut *prevpipe, &shell.env);
+			}
+			if do_redirections(&mut shell.token_vec[i].cmd_args_vec).is_err() {
+				shell.restore();
+				eprint_msh!("failed to do redirections");
+				std::process::exit(1);
+			}
+			libc::dup2(*prevpipe, 0);
+			libc::close(*prevpipe);
+			executor(&mut shell.token_vec[i], &mut shell.env);
+			std::process::exit(shell.env.get_status());
 		}
-		if do_redirections(&mut shell.token_vec[i].cmd_args_vec).is_err() {
-			shell.restore();
-			eprint_msh!("failed to do redirections");
-			std::process::exit(1);
-		}
-		dup2(*prevpipe, 0);
-		close(*prevpipe);
-		executor(&mut shell.token_vec[i], &mut shell.env);
-		std::process::exit(shell.env.get_status());
-	} else {
-		waitpid(cpid, &mut status, 0);
-		close(*prevpipe);
-		while wait(std::ptr::null_mut::<i32>()) > 0 {}
-		if status & 0x7f == 0 {
-			shell.env.set_status(((status & 0xff00) >> 8) as u8 as i32);
-		}
-	};
+		Err(e) => eprintln!("fork failed: {}", e),
+	}
 }
 
 unsafe fn exec_pipe(shell: &mut t_shell, i: usize, prevpipe: *mut i32) {
 	let mut pipefd: [i32; 2] = [0; 2];
-	pipe(pipefd.as_mut_ptr());
-	let cpid = fork();
-	if cpid == 0 {
-		// check_signals_child(&mut (*shell).p_termios);
-		close(pipefd[0_usize]);
-		dup2(pipefd[1_usize], 1);
-		close(pipefd[1_usize]);
-		dup2(*prevpipe, 0);
-		close(*prevpipe);
-		if let Err(status) = do_redirections(&mut shell.token_vec[i].cmd_args_vec) {
-			eprint_msh!("failed to do redirections");
-			std::process::exit(status);
+	libc::pipe(pipefd.as_mut_ptr());
+	match unsafe { fork() } {
+		Ok(ForkResult::Parent { .. }) => {
+			libc::close(pipefd[1_usize]);
+			libc::close(*prevpipe);
+			*prevpipe = pipefd[0_usize];
 		}
-		executor(&mut shell.token_vec[i], &mut shell.env);
-		std::process::exit(shell.env.get_status());
-	} else {
-		close(pipefd[1_usize]);
-		close(*prevpipe);
-		*prevpipe = pipefd[0_usize];
+		Ok(ForkResult::Child) => {
+			// check signals child
+			libc::close(pipefd[0_usize]);
+			libc::dup2(pipefd[1_usize], 1);
+			libc::close(pipefd[1_usize]);
+			libc::dup2(*prevpipe, 0);
+			libc::close(*prevpipe);
+			if let Err(status) = do_redirections(&mut shell.token_vec[i].cmd_args_vec) {
+				eprint_msh!("failed to do redirections");
+				std::process::exit(status);
+			}
+			executor(&mut shell.token_vec[i], &mut shell.env);
+			std::process::exit(shell.env.get_status());
+		}
+		Err(e) => eprintln!("fork failed: {}", e),
 	};
 }
 
